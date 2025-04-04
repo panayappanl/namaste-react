08657dbe466354141f6d61d508b07cd9f23b778d
panayappan02 <panayappan02@gmail.com>
2025-04-04 10:24:28 +0530

    Added: Ignited Our App through parcel

--- app.js
+++ app.js
@@ -15,10 +15,13 @@
  *ReactElement (Object) => HTML (Browser Understands)
  */
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
+    React.createElement("h1", {}, "This is Namaste React 🚀"),
+    React.createElement("h2", {}, "By Akshay Saini"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I'm h1 tag"),
