17206894cb73b090a4ae20eb644a735a5ba873de
panayappan02 <panayappan02@gmail.com>
2025-07-06 14:11:37 +0530

    Added: Ignited our App

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-// React Element -> Object => HTML(Browser Understands)
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1 Tag"),
-    React.createElement("h2", {}, "I'm h2 Tag"),
+  React.createElement("div", { id: "child", key: "child1" }, [
+    React.createElement("h1", { key: "child1-1" }, "This is Namaste React 🚀"),
+    React.createElement("h2", { key: "child1-2" }, "I'm h2 Tag"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm h1 Tag"),
-    React.createElement("h2", {}, "I'm h2 Tag"),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", { key: "child2-1" }, "I'm h1 Tag"),
+    React.createElement("h2", { key: "child2-2" }, "I'm h2 Tag"),
   ]),
 ]);
-
-console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
